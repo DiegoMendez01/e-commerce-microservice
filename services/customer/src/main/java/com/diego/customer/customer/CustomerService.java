e2982fc75b71d54362ce272205c8436b4f2913a1
DiegoMendez01 <diegomendez01soft@gmail.com>
2025-05-18 16:52:45 -0500

    Add customer service

--- services/customer/src/main/java/com/diego/customer/customer/CustomerService.java
+++ services/customer/src/main/java/com/diego/customer/customer/CustomerService.java
@@ -1,6 +1,5 @@
 package com.diego.customer.customer;
 
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
