e03783f2f7464e19e7cba6590b6af6d7746d4dce
DiegoMendez01 <diegomendez01soft@gmail.com>
2025-05-18 18:48:16 -0500

    Generate migration db product

---
+++ services/product/src/main/java/com/diego/product/category/Category.java
@@ -0,0 +1,27 @@
+package com.diego.product.category;
+
+import com.diego.product.product.Product;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+@Entity
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private int id;
+
+    private String name;
+
+    private String description;
+
+    @OneToMany(mappedBy = "category", cascade = CascadeType.REMOVE)
+    private List<Product> products;
+}
