579c943c257b9bd63989a52288529b947cae7a4a
DiegoMendez01 <diegomendez01soft@gmail.com>
2025-06-14 20:55:04 -0500

    Add Cors Filter

---
+++ services/api-gateway/src/main/java/com/diego/ecommerce/config/CustomCorsFilter.java
@@ -0,0 +1,41 @@
+package com.diego.ecommerce.config;
+
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ServerWebExchange;
+
+@Configuration
+public class CustomCorsFilter {
+
+    @Bean
+    @Order(Ordered.HIGHEST_PRECEDENCE)
+    public GlobalFilter corsGlobalFilter() {
+        return (exchange, chain) -> {
+            String origin = exchange.getRequest().getHeaders().getOrigin();
+
+            if (origin != null && origin.equals("http://localhost:5173")) {
+                HttpHeaders headers = exchange.getResponse().getHeaders();
+
+                if (!headers.containsKey("Access-Control-Allow-Origin")) {
+                    headers.add("Access-Control-Allow-Origin", origin);
+                }
+                headers.add("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+                headers.add("Access-Control-Allow-Headers", "*");
+                headers.add("Access-Control-Allow-Credentials", "true");
+
+                if (exchange.getRequest().getMethod() == HttpMethod.OPTIONS) {
+                    exchange.getResponse().setStatusCode(HttpStatus.OK);
+                    return exchange.getResponse().setComplete();
+                }
+            }
+
+            return chain.filter(exchange);
+        };
+    }
+}
